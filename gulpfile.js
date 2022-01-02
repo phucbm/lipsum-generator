d5c42fbd060d814dd44dd7435d27fe0b482a3fea
phucbm <hiphucbui@gmail.com>
2022-01-02 22:04:17 +0700

    build: modify gulp to export extension

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-const gulp = require('gulp'),
-    browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const zip = require('gulp-zip');
+const del = require('del');
+const version = '3.0.0';
+
 
 /**
- * Serve
+ * Serve: gulp serve
  */
 gulp.task('serve', function(){
     gulp.watch('**').on('change', () => {
@@ -13,3 +17,33 @@ gulp.task('serve', function(){
         server: ["./"]
     });
 });
+
+
+/**
+ * Export extension: gulp export
+ */
+const files = [
+    'assets/**/*.*',
+    'index.html',
+    'manifest.json'
+]
+
+// move files to temp folder
+gulp.task('dist', () => {
+    return gulp.src(files, {base: './'})
+        .pipe(gulp.dest('dist'))
+});
+
+// zip temp folder
+gulp.task('zip', () => {
+    return gulp.src(['dist/**/*.*'], {base: './'})
+        .pipe(zip(`lipsum-extension-${version}.zip`))
+        .pipe(gulp.dest('extension'));
+});
+
+// delete temp folder
+gulp.task('clean', function(){
+    return del('dist', {force: true});
+});
+
+gulp.task('export', gulp.series('dist', 'zip', 'clean'));
