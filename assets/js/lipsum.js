f7f5bd11410073e3c434e762fd93c65678ed3451
phucbm <hiphucbui@gmail.com>
2022-01-02 10:46:18 +0700

    feat(core): lipsum core

--- assets/js/lipsum.js
+++ assets/js/lipsum.js
@@ -4,101 +4,127 @@
  * MIT License - Copyright (c) 2022 Minh-Phuc Bui
  */
 ;(function(Lipsum){
-    const types = ['word', 'sentence', 'paragraph'];
-    const sourceString = 'lorem ipsum dolor sit amet consectetur adipiscing elit integer nec odio praesent libero sed cursus ante dapibus diam nisi nulla quis sem at nibh elementum imperdiet duis sagittis mauris fusce tellus augue semper porta massa vestibulum lacinia arcu eget class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos curabitur sodales ligula in dignissim nunc tortor pellentesque aenean quam scelerisque maecenas mattis convallis tristique proin ut vel egestas porttitor morbi lectus risus iaculis suscipit luctus non ac turpis aliquet metus ullamcorper tincidunt euismod quisque volutpat condimentum velit nam urna neque a facilisi fringilla suspendisse potenti feugiat mi consequat sapien etiam ultrices justo eu magna lacus vitae pharetra auctor interdum primis faucibus orci et posuere cubilia curae molestie dui blandit congue pede facilisis laoreet donec viverra malesuada enim est pulvinar sollicitudin cras id nisl felis venenatis commodo ultricies accumsan pretium fermentum nullam purus aliquam mollis vivamus consectetuer si leo eros maximus gravida erat letius ex hendrerit lobortis tempus rutrum efficitur phasellus natoque penatibus magnis dis parturient montes nascetur ridiculus mus vehicula bibendum vulputate dictum finibus eleifend rhoncus placerat tempor ornare hac habitasse platea dictumst habitant senectus netus fames';
-    const sourceArray = parseString(sourceString);
-    const config = {
-        'word': {quantity: 5},
-        'sentence': {quantity: 3, from: 6, to: 12},
-        'paragraph': {quantity: 2, from: 4, to: 8}
-    };
+    class LipsumCore{
+        constructor(config){
+            this.types = ['word', 'sentence', 'paragraph'];
+            this.defaultQuantity = {
+                'word': 5,
+                'sentence': 3,
+                'paragraph': 2
+            };
+            this.typeRandomRange = {
+                'word': {},
+                'sentence': {from: 6, to: 12},
+                'paragraph': {from: 4, to: 8}
+            };
 
+            this.options = {
+                ...{
+                    sourceString: 'lorem ipsum dolor sit amet consectetur adipiscing elit integer nec odio praesent libero sed cursus ante dapibus diam nisi nulla quis sem at nibh elementum imperdiet duis sagittis mauris fusce tellus augue semper porta massa vestibulum lacinia arcu eget class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos curabitur sodales ligula in dignissim nunc tortor pellentesque aenean quam scelerisque maecenas mattis convallis tristique proin ut vel egestas porttitor morbi lectus risus iaculis suscipit luctus non ac turpis aliquet metus ullamcorper tincidunt euismod quisque volutpat condimentum velit nam urna neque a facilisi fringilla suspendisse potenti feugiat mi consequat sapien etiam ultrices justo eu magna lacus vitae pharetra auctor interdum primis faucibus orci et posuere cubilia curae molestie dui blandit congue pede facilisis laoreet donec viverra malesuada enim est pulvinar sollicitudin cras id nisl felis venenatis commodo ultricies accumsan pretium fermentum nullam purus aliquam mollis vivamus consectetuer si leo eros maximus gravida erat letius ex hendrerit lobortis tempus rutrum efficitur phasellus natoque penatibus magnis dis parturient montes nascetur ridiculus mus vehicula bibendum vulputate dictum finibus eleifend rhoncus placerat tempor ornare hac habitasse platea dictumst habitant senectus netus fames',
+                    prefixString: 'lorem ipsum dolor sit amet',
+                    type: 'word', // default type
+                    quantity: this.defaultQuantity[config.type]
+                }, ...config
+            };
 
-    /**
-     * Get array of word|sentence|paragraph
-     * @param type
-     * @param quantity
-     * @returns {*}
-     */
-    function getArray(type = 'word', quantity = 5){
-        if(!types.includes(type)){
-            console.warn(`[${type}] is not supported!`);
-            return;
+            this.sourceArray = parseString(this.options.sourceString);
         }
 
-        quantity = quantity ?? config[type].quantity;
-        const from = config[type].from;
-        const to = config[type].to;
-        const result = [];
+        /**
+         * Get array of word|sentence|paragraph
+         * @param type
+         * @param quantity
+         * @returns {*}
+         */
+        getArray(type = this.options.type, quantity = this.options.quantity){
+            if(!this.types.includes(type)){
+                console.warn(`[${type}] is not supported!`);
+                return;
+            }
 
-        switch(type){
-            case "word":
-                let word = '';
-                while(result.length < quantity){
-                    word = sourceArray[random(0, sourceArray.length - 1)];
-                    if(!result.includes(word)){
-                        result.push(word);
+            const from = this.typeRandomRange[type].from;
+            const to = this.typeRandomRange[type].to;
+            const result = [];
+
+            switch(type){
+                case "word":
+                    let word = '';
+                    while(result.length < quantity){
+                        word = this.sourceArray[random(0, this.sourceArray.length - 1)];
+                        if(!result.includes(word)){
+                            result.push(word);
+                        }
                     }
-                }
-                break;
-            case "sentence":
-                while(result.length < quantity){
-                    result.push(getArray('word', random(from, to)));
-                }
-                break;
-            case "paragraph":
-                while(result.length < quantity){
-                    result.push(getArray('sentence', random(from, to)));
-                }
-                break;
+                    break;
+                case "sentence":
+                    while(result.length < quantity){
+                        result.push(this.getArray('word', random(from, to)));
+                    }
+                    break;
+                case "paragraph":
+                    while(result.length < quantity){
+                        result.push(this.getArray('sentence', random(from, to)));
+                    }
+                    break;
+            }
+
+            return result;
         }
 
-        return result;
-    }
 
+        /**
+         * Get string from array of word|sentence|paragraph
+         * @param type
+         * @param quantity
+         * @param array
+         * @returns {string}
+         */
+        getString(type = this.options.type, quantity = this.options.quantity, array = []){
+            array = array.length ? array : this.getArray(type, quantity);
+            let result = '';
 
-    /**
-     * Get string from array of word|sentence|paragraph
-     * @param type
-     * @param quantity
-     * @param array
-     * @returns {string}
-     */
-    function getString(type = 'word', quantity = 5, array = []){
-        array = array.length ? array : getArray(type, quantity);
-        let result = '';
+            switch(type){
+                case "word":
+                    for(const word of array) result += word + ' ';
+                    break;
+                case "sentence":
+                    for(const words of array){
+                        result += capitalizeFirstLetter(this.getString("word", quantity, words)) + '. ';
+                    }
+                    break;
+                case "paragraph":
+                    for(const sentences of array){
+                        result += this.getString("sentence", quantity, sentences) + '\n\n';
+                    }
+                    break;
+            }
 
-        switch(type){
-            case "word":
-                for(const word of array) result += word + ' ';
-                break;
-            case "sentence":
-                for(const words of array){
-                    result += capitalizeFirstLetter(getString("word", quantity, words)) + '. ';
-                }
-                break;
-            case "paragraph":
-                for(const sentences of array){
-                    result += getString("sentence", quantity, sentences) + '\n\n';
-                }
-                break;
+            // remove the last white space
+            result = result.trim();
+
+            return result;
         }
-
-        // remove the last white space
-        result = result.trim();
-
-        return result;
     }
 
 
     /**
      * Get lipsum
-     * @param type
-     * @param quantity
+     * @param config
      * @returns {string}
      */
-    Lipsum.get = (type = 'word', quantity = 5) => {
-        return getString(type, quantity);
+    Lipsum.get = (config) => {
+        const options = {
+            ...{
+                type: 'word',
+                quantity: 5,
+                hasPrefix: false
+            }, ...config
+        };
+
+        const lipsum = new LipsumCore(options);
+        console.log(options)
+
+        return lipsum.getString();
     };
 
 })(window.Lipsum = window.Lipsum || {});
